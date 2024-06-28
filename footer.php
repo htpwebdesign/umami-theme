0919efc5e2a2cbcc2a62485a49000fe6e3872c73
Catharina <144069315+cgtrust@users.noreply.github.com>
2024-06-28 14:40:30 -0700

    removed site info from the footer

--- footer.php
+++ footer.php
@@ -22,19 +22,6 @@
 			);
 			?>
 		</nav>
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'umami-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'umami-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'umami-theme' ), 'umami-theme', '<a href="https://wp.bcitwebdeveloper.ca/">FWD 37 - Team 2</a>' );
-				?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
