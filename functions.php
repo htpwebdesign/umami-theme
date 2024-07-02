6ab8456136b43bdce80db054c00b5e68ec000650
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-07-02 10:46:32 -0700

    removed a tag from product images, single product page no longer avaliable!

--- functions.php
+++ functions.php
@@ -225,3 +225,6 @@ function remove_all_wc_add_to_cart() {
     remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 }
+
+remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
