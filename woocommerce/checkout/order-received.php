99991ed9298d90189bf506e3e9e8050a75df10a9
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-07-16 15:37:39 -0700

    modified order received

--- woocommerce/checkout/order-received.php
+++ woocommerce/checkout/order-received.php
@@ -21,40 +21,26 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received">
-    <?php
-    // Default message
-    $message = esc_html__( 'Thank you. Your order has been received. Our orders should take up to 30-45 minutes to complete, but during peak hours it could take up to an hour.', 'woocommerce' );
+	<?php
+	/**
+	 * Filter the message shown after a checkout is complete.
+	 *
+	 * @since 2.2.0
+	 *
+	 * @param string         $message The message.
+	 * @param WC_Order|false $order   The order created during checkout, or false if order data is not available.
+	 */
 
-    if ( $order ) {
-        // Get shipping method ID
-        $shipping_method = '';
-        foreach ( $order->get_items('shipping') as $item_id => $shipping_item ) {
-            $shipping_method = $shipping_item->get_method_id();
-        }
+	// Output the shipping method(s) formatted method title(s)
+	$shipping_method = $order->get_shipping_method();
 
-        // Customize message based on shipping method
-        if ( $shipping_method == 'local_pickup' ) {
-            $message = esc_html__( 'Thank you for your order! Your items will be ready for pickup in 15 minutes, but during peak hours it may take up to 45 minutes. We thank you for your patience.', 'woocommerce' );
-        } elseif ( $shipping_method == 'free_shipping' ) {
-            $message = esc_html__( 'Thank you for your order! Your items will be delivered to you in 30 to 45 minutes, but during peak hours it may take up to 1 hour. We thank you for your patience.', 'woocommerce' );
-        }
-    }
+	$message = apply_filters(
+		'woocommerce_thankyou_order_received_text',
+		esc_html( __( 'Thank you. Your order has been received. Our orders should take up to 30-45 minutes to complete, but during peak hours it could take up to an hour.', 'woocommerce' ) ),
+		$order
+	);
 
-    /**
-     * Filter the message shown after a checkout is complete.
-     *
-     * @since 2.2.0
-     *
-     * @param string         $message The message.
-     * @param WC_Order|false $order   The order created during checkout, or false if order data is not available.
-     */
-    $message = apply_filters(
-        'woocommerce_thankyou_order_received_text',
-        $message,
-        $order
-    );
-
-    // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-    echo $message;
-    ?>
+	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	echo $message . $shipping_method;
+	?>
 </p>
